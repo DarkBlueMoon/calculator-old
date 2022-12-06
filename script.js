c5dd30c391ca698857c68b42b0e11628c284b078
Demetri Evans <3819574+DarkBlueMoon@users.noreply.github.com>
2022-12-06 16:24:57 -0600

    Operate function for later use

--- script.js
+++ script.js
@@ -15,3 +15,18 @@ function divide(num1, num2) {
 
   return Math.round((num1 / num2) * 100) / 100;
 }
+
+function operate(op, num1, num2) {
+  switch (op) {
+    case "+":
+      return add(num1, num2);
+    case "-":
+      return subtract(num1, num2);
+    case "*":
+      return multiply(num1, num2);
+    case "/":
+      return divide(num1, num2);
+    default:
+      break;
+  }
+}
