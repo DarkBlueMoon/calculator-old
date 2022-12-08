4e08c5a650c1ccc50d336a7f55664e8fe29310b8
Demetri Evans <3819574+DarkBlueMoon@users.noreply.github.com>
2022-12-07 20:00:49 -0600

    User can chain operators (ex. 12+7-5*3)

--- script.js
+++ script.js
@@ -3,22 +3,57 @@ const operatorBtns = document.querySelectorAll(".operator");
 const eqBtn = document.querySelector(".equals");
 const resultDisplay = document.getElementById("result");
 const clearBtn = document.querySelector(".clear");
+let operand1 = "";
+let operand2 = "";
 let displayOperand = 0;
 let currOperator = "";
 let showAnswer = false;
 let clearFlag = false;
+let finalCalc = false; // Potentially replacing showAnswer
+// finalCalc means that = has been pressed and we are no longer calculating for this current iteration.
+// Two operand vars (if something in both, update display w/ current calc)
+let currentlyCalcing = true;
 
 numberBtns.forEach((btn) => {
   btn.addEventListener("click", () => {
+    if (clearFlag) {
+      clearDisplay();
+    }
     updateDisplay(btn.textContent);
   });
 });
 
 operatorBtns.forEach((btn) => {
   btn.addEventListener("click", () => {
-    displayOperand = parseInt(resultDisplay.value);
-    currOperator = btn.textContent;
-    resultDisplay.value = "";
+    // Default case: operand1 and operand2 are empty
+    if (operand1 === "") {
+      operand1 = resultDisplay.value;
+      currOperator = btn.textContent;
+    } else if (operand1 !== "") {
+      operand2 = resultDisplay.value;
+      clearDisplay();
+      operand1 = operate(currOperator, operand1, operand2);
+      updateDisplay(operand1);
+      currOperator = btn.textContent;
+    }
+    clearFlag = true;
+
+    // if (operand1 !== "" && operand2 !== "") {
+    //   // operand1 = resultDisplay.value;
+    //   // currOperator = btn.textContent;
+    //   // clearFlag = true; // clear display on next btn press
+    // }
+    // if (finalCalc) {
+    // } else {
+    //   displayOperand += parseInt(resultDisplay.value);
+    //   currOperator = btn.textContent;
+    //   clearFlag = true;
+    // }
+    // updateDisplay(displayOperand);
+    // displayOperand = parseInt(resultDisplay.value);
+    // currOperator = btn.textContent;
+    // // resultDisplay.value = "";
+    // clearFlag = true;
   });
 });
 
@@ -27,9 +62,33 @@ eqBtn.addEventListener("click", () => {
   updateDisplay(
     operate(currOperator, displayOperand, parseInt(resultDisplay.value))
   );
+  clearFlag = true;
 });
 
 clearBtn.addEventListener("click", resetCalc);
+
+function updateDisplay(val) {
+  if (showAnswer) {
+    resultDisplay.value = val;
+    showAnswer = false;
+    return;
+  }
+
+  resultDisplay.value = resultDisplay.value + val;
+}
+
+function resetCalc() {
+  resultDisplay.value = "";
+  displayOperand = 0;
+  currOperator = "";
+  showAnswer = false;
+  clearFlag = false;
+}
+
+function clearDisplay() {
+  resultDisplay.value = "";
+  clearFlag = false;
+}
 
 function add(num1, num2) {
   return num1 + num2;
@@ -50,6 +109,9 @@ function divide(num1, num2) {
 }
 
 function operate(op, num1, num2) {
+  num1 = parseFloat(num1);
+  num2 = parseFloat(num2);
+
   switch (op) {
     case "+":
       return add(num1, num2);
@@ -62,22 +124,4 @@ function operate(op, num1, num2) {
     default:
       break;
   }
-}
-
-function updateDisplay(val) {
-  if (showAnswer) {
-    resultDisplay.value = val;
-    showAnswer = false;
-    return;
-  }
-
-  resultDisplay.value = resultDisplay.value + val;
-}
-
-function resetCalc() {
-  resultDisplay.value = "";
-  displayOperand = 0;
-  currOperator = "";
-  showAnswer = false;
-  clearFlag = false;
 }
